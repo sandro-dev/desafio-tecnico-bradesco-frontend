8c9ac992df65fbc777e9f1b6945164da050e5c50
Sandro Santos <sandro.dev@gmail.com>
2021-02-19 19:10:32 -0300

    feat: about page

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
-import { Container } from './styles';
+import { Container, Content } from './styles';
+
+import Header from '../../components/Header';
 
 const About: React.FC = () => {
   return (
     <Container>
-      <h1>About</h1>
+      <Header />
+      <Content>
+        <h1>Sobre</h1>
+        <p>
+          Esta é uma aplicação front-end feita em ReactJS para o desafio técnico
+          Bradesco Seguros
+        </p>
+      </Content>
     </Container>
   );
 };
--- src/pages/About/styles.ts
+++ src/pages/About/styles.ts
@@ -1,3 +1,42 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  background: #fff;
+  max-width: 1120px;
+  margin: 0 auto;
+`;
+export const Content = styled.div`
+  padding: 30px;
+
+  h1,
+  h2,
+  p {
+    margin: 10px 0;
+  }
+
+  p {
+    color: #333;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    strong {
+      font-family: Quicksand;
+      font-weight: bold;
+      font-size: 22px;
+    }
+
+    button {
+      background: #b31634;
+      padding: 16px;
+      border-radius: 50%;
+      margin: 20px;
+    }
+  }
+`;
