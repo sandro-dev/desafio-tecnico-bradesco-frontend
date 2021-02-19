c82b7c1cbc2722d828dfec8324853984e528d6e4
Sandro Santos <sandro.dev@gmail.com>
2021-02-19 16:11:03 -0300

    style: global style updates

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -10,15 +10,20 @@ export default createGlobalStyle`
     }
 
     body {
-      font-family: 'Quicksand', sans-serif;
+      font-family: Quicksand;
       font-size: 16px;
       background: #f5f5f5;
       color: #b31634;
     }
 
     h1, h2, h3, h4, h5, h6 {
-      font-weight: 500;
+      font-family: Quicksand;
+      font-weight: 700;
     }
+
+    h1 { font-size: 32px;}
+    h2 { font-size: 16px;}
+    h3 { font-size: 12px; font-weight: 300; }
 
     button {
       cursor: pointer;
