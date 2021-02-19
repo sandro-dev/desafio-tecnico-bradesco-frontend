68915d6b9b24cba6963a12b48823bde0d97a0d1f
Sandro Santos <sandro.dev@gmail.com>
2021-02-19 16:10:40 -0300

    feat: homepage with icon chat

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { IoChatbubblesOutline } from 'react-icons/io5';
 
-import { Container } from './styles';
+import api from '../../services/api';
+
+import logo from '../../assets/logo-bradesco-seguros.svg';
+
+import { Container, Header, Content } from './styles';
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    async function loadBots() {
+      const response = await api.get('/bots');
+      console.log(response.data);
+    }
+    loadBots();
+  }, []);
+
   return (
     <Container>
-      <h1>Home</h1>
+      <Header>
+        <img src={logo} alt="Bradesco Seguros" width="30%" />
+      </Header>
+      <Content>
+        <h1>Produtos Bradesco Seguros</h1>
+        <h2>Seguro Auto</h2>
+        <p>
+          O Bradesco Seguro Auto oferece coberturas básicas e adicionais, além
+          de serviços que proporcionam conforto e segurança para você e sua
+          família. Veja algumas: Reboque Assistência Auto Dia e Noite com
+          diversas opções de limites de quilometragens Pagamento Escolha o
+          melhor dia para seu pagamento Carro Reserva Três opções de diárias: 7,
+          15 ou 30
+        </p>
+        <h2>Seguro Residencial</h2>
+        <p>
+          É um seguro abrangente, flexível, e custa menos do que você imagina
+          Você assegura sua residência habitual, construída em alvenaria,
+          própria ou alugada, com uma série de coberturas, além de contar com
+          uma extensa rede de assistência. E nem é preciso fazer vistoria do
+          imóvel.
+        </p>
+        <h2>Seguro Viagem</h2>
+        <p>
+          Seguro Viagem Bradesco garante sua proteção e proporciona muito mais
+          tranquilidade em suas viagens de trabalho e lazer. Além de conforto de
+          um pacote de benefícios.
+        </p>
+
+        <div>
+          <strong>Tire suas dúvidas agora mesmo</strong>
+          <button type="button">
+            <IoChatbubblesOutline size={64} color="#fff" />
+          </button>
+        </div>
+      </Content>
     </Container>
   );
 };
--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -1,3 +1,47 @@
 import styled from 'styled-components';
 
-export const Container = styled.div``;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+
+  background: #fff;
+  max-width: 900px;
+  margin: 0 auto;
+`;
+
+export const Header = styled.header`
+  padding: 32px;
+`;
+
+export const Content = styled.div`
+  padding: 30px;
+
+  h1,
+  h2,
+  p {
+    margin: 10px 0;
+  }
+
+  p {
+    color: #333;
+  }
+
+  div {
+    display: flex;
+    align-items: center;
+
+    strong {
+      font-family: Quicksand;
+      font-weight: bold;
+      font-size: 22px;
+    }
+
+    button {
+      background: #b31634;
+      padding: 16px;
+      border-radius: 50%;
+      margin: 20px;
+    }
+  }
+`;
