276e33b93d0f559ed2e8c9de1ab85893831bac6d
Sandro Santos <sandro.dev@gmail.com>
2021-02-22 19:37:50 -0300

    refactor: now session hook has responsability for data session

--- src/hooks/Chat.tsx
+++ src/hooks/Chat.tsx
@@ -18,16 +18,9 @@ export interface ChatMessage {
   text: string;
 }
 
-interface ChatDTO {
-  conversationId: string;
-  userId: string;
-  botId: string;
-}
-
 interface ChatContextData {
   addMessage(message: Omit<ChatMessage, 'id'>): Promise<void>;
   toggleChat(isClosed: boolean): void;
-  chatProps: ChatDTO;
   loading?: boolean;
 }
 
@@ -37,83 +30,39 @@ const ChatProvider: React.FC = ({ children }) => {
   const [messages, setMessages] = useState<ChatMessage[]>([]);
   const [loading, setLoading] = useState(true);
 
-  const [chatProps, setChatProps] = useState<ChatDTO>(() => {
-    const conversationId = localStorage.getItem('@ChatBradesco:conversationId');
-    const userId = localStorage.getItem('@ChatBradesco:userId');
-    const botId = localStorage.getItem('@ChatBradesco:botId');
-
-    // if already exists on localStorage, return data
-    if (conversationId && userId && botId) {
-      return { conversationId, userId, botId };
-    }
-    // if not exists, return empty object
-    return {} as ChatDTO;
-  });
-
-  useEffect(() => {
-    async function loadChat() {
-      if (chatProps.conversationId) {
-        const response = await api.get('/messages', {
-          params: { conversationId: chatProps.conversationId },
-        });
-        setMessages(response.data);
-      }
-    }
-
-    let expiresIn = localStorage.getItem('@ChatBradesco:expiresIn');
-
-    if (expiresIn) {
-      const now = Date.now();
-      if (now > Number(expiresIn)) {
-        localStorage.clear();
-      } else {
-        loadChat();
-      }
-    } else {
-      const date = new Date();
-      date.setHours(date.getHours() + 1);
-      expiresIn = String(date.getTime());
-      localStorage.setItem('@ChatBradesco:expiresIn', String(expiresIn));
-      loadChat();
-    }
-
-    setLoading(false);
-  }, [chatProps.conversationId]);
-
-  const persistData = useCallback(
-    ({ conversationId, userId, botId }: ChatDTO) => {
-      localStorage.setItem('@ChatBradesco:conversationId', conversationId);
-      localStorage.setItem('@ChatBradesco:userId', userId);
-      localStorage.setItem('@ChatBradesco:botId', botId);
-    },
-    [],
-  );
-
   const addMessage = useCallback(
     async ({ conversationId, from, to, text }: Omit<ChatMessage, 'id'>) => {
       const newMessage = { id: uuid(), conversationId, from, to, text };
 
       const response = await api.post('/messages', newMessage);
-      const { conversationId: chatId, from: userId, to: botId } = response.data;
-
-      setChatProps({ conversationId: chatId, userId, botId });
       setMessages(state => [...state, response.data]);
-
-      if (!chatProps.conversationId) {
-        persistData({ conversationId: chatId, userId, botId });
-      }
     },
-    [persistData, chatProps.conversationId],
+    [],
   );
 
   const toggleChat = useCallback((isClosed: boolean) => {
     localStorage.setItem('@ChatBradesco:chatIsClosed', String(isClosed));
   }, []);
 
+  useEffect(() => {
+    const conversationId = localStorage.getItem('@ChatBradesco:conversationId');
+
+    async function loadChat() {
+      const response = await api.get('/messages', {
+        params: { conversationId },
+      });
+      setMessages(response.data);
+    }
+
+    // if conversationId, loadMessage
+    if (conversationId) {
+      loadChat();
+    }
+    setLoading(false);
+  }, []);
+
   return (
-    <ChatContext.Provider
-      value={{ addMessage, chatProps, loading, toggleChat }}
-    >
+    <ChatContext.Provider value={{ addMessage, loading, toggleChat }}>
       {children}
       <Chat messages={messages} />
     </ChatContext.Provider>
